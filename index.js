6cf141793ab11b707c5c4db183d7e5762451966a
EnriqueML <enriquem1990@hotmail.com>
2018-06-21 17:00:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -26,6 +26,52 @@ server.post('/get-movie-details', (req, res) => {
             let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
             dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
+            return res.json({
+                speech: dataToSend,
+                displayText: dataToSend,
+                source: 'get-movie-details'
+            });
+        });
+    }, (error) => {
+        return res.json({
+            speech: 'Something went wrong!',
+            displayText: 'Something went wrong!',
+            source: 'get-movie-details'
+        });
+    });
+});
+
+server.listen((process.env.PORT || 8000), () => {
+    console.log("Server is up and running...");
+});*/
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const API_KEY = require('./apiKey');
+
+const server = express();
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+server.use(bodyParser.json());
+
+server.post('/get-movie-details', (req, res) => {
+
+    const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.q ? req.body.result.parameters.q : 'madrid';
+    const reqUrl = `http://api.openweathermap.org/data/2.5/weather?q=${movieToSearch}&appid=6a7f545ebdbdfb2d3beb7cfab9f2e975&units=metric`;
+    http.get(reqUrl, (responseFromAPI) => {
+        let completeResponse = '';
+        responseFromAPI.on('data', (chunk) => {
+            completeResponse += chunk;
+        });
+        responseFromAPI.on('end', () => {  
+            let response = JSON.parse(completeResponse);
+            let weather = response['weather'][0]['description'];
+            let temp = response['main']['temp'];
+            let dataToSend = `El tiempo es de ${weather} con una temperatura de ${temp}.`;
             return res.json({
                 speech: dataToSend,
                 displayText: dataToSend,
